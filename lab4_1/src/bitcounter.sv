f1b47aa1c2866a832115f791d0e5e1f42b715b7b
luke-jiang <yuxuanlukejiang@gmail.com>
2019-03-02 21:33:43 -0800

    remove duplicate bitcounter

--- lab4_1/src/bitcounter.sv
+++ lab4_1/src/bitcounter.sv
@@ -1,14 +1,23 @@
+// CSE 371
+// Lab 4 Part 1
+// Jie Deng, Luke Jiang
+// 21/02/2019
+
+// Implement the bit-counting ASM described in the spec.
+// Count how many ones in an n-bit input A.
+
 module bitcounter (
 	output logic [3:0] result,
 	output logic done,
 	input logic [7:0] A,
 	input logic clk, reset, start
 	);
+
 	logic [7:0] Ar;
 	logic incr_result, rshift_A, init_result, init_A;
 	bitcounter_controller C (.*);
 	bitcounter_datapath D (.*);
-	
+
 endmodule
 
 module bitcounter_controller (
@@ -16,32 +25,32 @@ module bitcounter_controller (
 	input logic start, clk, reset,
 	input logic [7:0] Ar
 	);
-	
+
 	enum {S1, S2, S3} ps, ns;
-	
+
 	always_comb begin
 		case (ps)
 			S1: ns = start ? S2 : S1;
 			S2: ns = (Ar == 0) ? S3 : S2;
 			S3: ns = start ? S3 : S1;
 		endcase
-	end 
-	
-	always_comb begin 
+	end
+
+	always_comb begin
 		incr_result = (ps == S2) & (ns == S2) & (Ar[0] == 1);
 		init_A = (ps == S1) & (~start);
 		rshift_A = (ps == S2);
 		done = (ps == S3);
 		init_result = (ps == S1);
-	end 
-	
+	end
+
 	always_ff @(posedge clk) begin
 		if (reset)
 			ps <= S1;
-		else 
+		else
 			ps <= ns;
-	end 
-endmodule 
+	end
+endmodule
 
 module bitcounter_datapath (
 	output logic [7:0] Ar,
@@ -51,7 +60,7 @@ module bitcounter_datapath (
 	input logic clk
 	);
 
-	always_ff @(posedge clk) begin 
+	always_ff @(posedge clk) begin
 		if (incr_result)
 			result <= result + 4'd1;
 		if (rshift_A)
@@ -60,22 +69,22 @@ module bitcounter_datapath (
 			Ar <= A;
 		if (init_result)
 			result <= 0;
-	end 
+	end
 endmodule
 
 module bitcounter_testbench ();
 	logic [3:0] result;
 	logic [7:0] A;
 	logic clk, reset, start, done;
-	
+
 	bitcounter dut (.*);
-	
+
 	parameter CLOCK_PERIOD = 100;
 	initial begin
 		clk <= 0;
 		forever #(CLOCK_PERIOD/2) clk <= ~clk;
 	end
-	
+
 	initial begin
 		reset <= 1; @(posedge clk);
 		reset <= 0; start <= 0; A <= 8'b1010_1010; @(posedge clk);
@@ -91,5 +100,5 @@ module bitcounter_testbench ();
 		@(posedge clk);
 		@(posedge clk);
 		$stop();
-	end 
+	end
 endmodule
