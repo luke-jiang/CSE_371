96023495b960d33b0dd525298ec07a14a5bfd050
luke-jiang <yuxuanlukejiang@gmail.com>
2019-03-02 21:52:43 -0800

    add comment

--- lab4_2/src/DE1_SoC.sv
+++ lab4_2/src/DE1_SoC.sv
@@ -1,3 +1,10 @@
+// CSE 371
+// Lab 4 Part 2
+// Jie Deng, Luke Jiang
+// 21/02/2019
+
+// instantiate searcher. Set up connections for display signals.
+
 module DE1_SoC (
 	output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5,
 	output logic [9:0] LEDR,
@@ -5,30 +12,29 @@ module DE1_SoC (
 	input logic [9:0] SW,
 	input logic CLOCK_50
 	);
-	
-	
+
 	logic found, done;
 	logic [4:0] addr;
 	logic [7:0] target;
-	
+
 	logic clk, reset, start;
-	
+
 	assign target = SW[7:0];
 	assign start = SW[9];
 	assign reset = ~KEY[0];
 	assign clk = CLOCK_50;
-	
+
 	assign LEDR[9] = found;
-	
+
 	logic [3:0] b0, b1;
 	assign b0 = addr % 5'd16;
 	assign b1 = addr / 5'd16;
-	
-	seg7 bit0(HEX0, b0);
-	seg7 bit1(HEX1, b1);
+
+	seg7 bit0 (HEX0, b0);
+	seg7 bit1 (HEX1, b1);
 	searcher Searcher (.*);
 endmodule
- 
+
 module DE1_SoC_testbench ();
 	logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
 	logic [9:0] LEDR;
@@ -43,8 +49,8 @@ module DE1_SoC_testbench ();
 		CLOCK_50 <= 0;
 		forever #(CLOCK_PERIOD/2) CLOCK_50 <= ~CLOCK_50;
 	end
-	
+
 	initial begin
-			
+
 	end
 endmodule
--- lab4_2/src/searcher.sv
+++ lab4_2/src/searcher.sv
@@ -1,18 +1,32 @@
+// CSE 371
+// Lab 4 Part 2
+// Jie Deng, Luke Jiang
+// 21/02/2019
+
+// implement a binary search module.
+
 `timescale 1 ps / 1 ps
+
 module searcher (
+	// searcher top module. Instantiate controller and datapath.
+	// use a wizard-generated RAM to store content to be searched.
+	
 	output logic found, done,
 	output logic [4:0] addr,
 	input logic [7:0] target,
 	input logic clk, reset, start
-);
+	);
+
 	logic [7:0] curr_data;
-	logic [4:0] new_addr;
-	logic [4:0] p1, p2;
+	logic [4:0] new_addr;  // (p1 + p2) / 2
+	logic [4:0] p1, p2;  // pointers specifying search range
+
+	// control signals
 	logic init_all, update_addr, set_p1, set_p2, set_found, set_done;
-	
+
 	searcher_controller C (.*);
 	searcher_datapath D (.*);
-	
+
 	ram32x8 RAM (
 		.q(curr_data),
 		.address(new_addr),
@@ -20,44 +34,46 @@ module searcher (
 		.data(),
 		.wren(1'b0)
 	);
-	
+
 endmodule
 
 module searcher_controller (
+	// implemented using a 4-state FSM.
+
 	output logic init_all, update_addr, set_p1, set_p2, set_found, set_done,
 	input logic [7:0] target,
 	input logic [7:0] curr_data,
 	input logic [4:0] p1, p2,
 	input logic clk, reset, start
 	);
-	
+
 	enum {IDLE, LOOP, WAIT, DONE} ps, ns;
-	
+
 	always_ff @(posedge clk) begin
 		if (reset)
 			ps <= IDLE;
 		else
 			ps <= ns;
-	end 
-	
+	end
+
 	always_comb begin
 		case (ps)
 			IDLE: ns = start ? LOOP : IDLE;
-			//LOOP: ns = ((p1 > p2) | (curr_data == target)) ? DONE : LOOP;
 			LOOP: ns = (p1 > p2) ? DONE : WAIT;
 			WAIT: ns = (curr_data == target) ? DONE : LOOP;
 			DONE: ns = start ? DONE : IDLE;
 		endcase
-		
+
+		// assign control signals
 		init_all = (ps == IDLE);
 		update_addr = (ps == LOOP);
 		set_p1 = (ps == LOOP) & (curr_data < target);
 		set_p2 = (ps == LOOP) & (curr_data > target);
 		set_found = (ps == LOOP) & (curr_data == target);
 		set_done = (ps == DONE);
-		
-	end 
-endmodule 
+
+	end
+endmodule
 
 module searcher_datapath (
 	output logic [4:0] p1, p2, addr,
@@ -66,17 +82,17 @@ module searcher_datapath (
 	input logic init_all, update_addr, set_p1, set_p2, set_found, set_done,
 	input logic clk
 	);
-	
+
 	assign new_addr = (p1 + p2) / 2;
-	
+
 	always_ff @(posedge clk) begin
-		if (init_all) begin 
+		if (init_all) begin
 			p1 <= 5'd0;
 			p2 <= 5'd31;
 			addr <= 5'd0;
 			done <= 0;
 			found <= 0;
-		end 
+		end
 		if (update_addr)
 			addr <= new_addr;
 		if (set_p1)
@@ -87,7 +103,7 @@ module searcher_datapath (
 			found <= 1;
 		if (set_done)
 			done <= 1;
-	end 
+	end
 endmodule
 
 module searcher_testbench ();
@@ -95,17 +111,17 @@ module searcher_testbench ();
 	logic [4:0] addr;
 	logic [7:0] target;
 	logic clk, reset, start;
-	
+
 	searcher dut (.*);
-	
+
 	parameter CLOCK_PERIOD = 100;
 	initial begin
 		clk <= 0;
 		forever #(CLOCK_PERIOD/2) clk <= ~clk;
 	end
-	
+
 	integer i;
-	initial begin 
+	initial begin
 		reset <= 1; @(posedge clk);
 		reset <= 0; target <= 8'd5; start <= 1; @(posedge clk);
 		for (i = 0; i < 32; i++)
