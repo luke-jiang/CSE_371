a15d7d6a44780a1205a3cd735be5df5aeebf7b22
luke-jiang <yuxuanlukejiang@gmail.com>
2019-03-02 22:03:36 -0800

    fix indent

--- lab4_1/src/DE1_SoC.sv
+++ lab4_1/src/DE1_SoC.sv
@@ -9,69 +9,69 @@
 // use switches as input
 
 module DE1_SoC (
-	output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5,
-	output logic [9:0] LEDR,
-	input logic [3:0] KEY,
-	input logic [9:0] SW,
-	input logic CLOCK_50
-	);
+  output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5,
+  output logic [9:0] LEDR,
+  input logic [3:0] KEY,
+  input logic [9:0] SW,
+  input logic CLOCK_50
+  );
 
-	logic [3:0] result;
-	logic done, start;
+  logic [3:0] result;
+  logic done, start;
 
-	assign HEX1 = 7'b1111111;
-	assign HEX2 = 7'b1111111;
-	assign HEX3 = 7'b1111111;
-	assign HEX4 = 7'b1111111;
-	assign HEX5 = 7'b1111111;
+  assign HEX1 = 7'b1111111;
+  assign HEX2 = 7'b1111111;
+  assign HEX3 = 7'b1111111;
+  assign HEX4 = 7'b1111111;
+  assign HEX5 = 7'b1111111;
 
-	assign start = SW[9];
-	assign reset = ~KEY[0];
-	assign LEDR[9] = done;
+  assign start = SW[9];
+  assign reset = ~KEY[0];
+  assign LEDR[9] = done;
 
-	// clock divider
-	// Fast clock may cause metastability issue
+  // clock divider
+  // Fast clock may cause metastability issue
   logic [31:0] divided_clk;
   logic clk;
   clock_divider getCLK (
-		.clock(CLOCK_50),
-		.divided_clocks(divided_clk)
+  	.clock(CLOCK_50),
+  	.divided_clocks(divided_clk)
   );
   assign clk = divided_clk[24];
 
-	bitcounter BC (.result, .done, .A(SW[7:0]), .clk, .reset, .start);
+  bitcounter BC (.result, .done, .A(SW[7:0]), .clk, .reset, .start);
 
-	seg7 result_dispay (.leds(HEX0), .bcd(result));
+  seg7 result_dispay (.leds(HEX0), .bcd(result));
 
 endmodule
 
 module DE1_SoC_testbench ();
-	logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
-	logic [9:0] LEDR;
-	logic [3:0] KEY;
-	logic [9:0] SW;
-	logic CLOCK_50;
+  logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+  logic [9:0] LEDR;
+  logic [3:0] KEY;
+  logic [9:0] SW;
+  logic CLOCK_50;
 
-	DE1_SoC dut (.*);
+  DE1_SoC dut (.*);
 
-	parameter CLOCK_PERIOD = 100;
-	initial begin
-		CLOCK_50 <= 0;
-		forever #(CLOCK_PERIOD/2) CLOCK_50 <= ~CLOCK_50;
-	end
+  parameter CLOCK_PERIOD = 100;
+  initial begin
+    CLOCK_50 <= 0;
+    forever #(CLOCK_PERIOD/2) CLOCK_50 <= ~CLOCK_50;
+  end
 
-	initial begin
-			SW <= 10'b0000000000; KEY[0] <= 0; @(posedge CLOCK_50);
-			KEY[0] <= 1; @(posedge CLOCK_50);
-			@(posedge CLOCK_50);
-			SW <= 10'b0000000111; @(posedge CLOCK_50);
-			SW[9] <= 1; @(posedge CLOCK_50);
-			@(posedge CLOCK_50);
-			@(posedge CLOCK_50);
-			@(posedge CLOCK_50);
-			@(posedge CLOCK_50);
-			@(posedge CLOCK_50);
-			@(posedge CLOCK_50);
-			$stop();
-	end
+  initial begin
+    SW <= 10'b0000000000; KEY[0] <= 0; @(posedge CLOCK_50);
+    KEY[0] <= 1; @(posedge CLOCK_50);
+    @(posedge CLOCK_50);
+    SW <= 10'b0000000111; @(posedge CLOCK_50);
+    SW[9] <= 1; @(posedge CLOCK_50);
+    @(posedge CLOCK_50);
+    @(posedge CLOCK_50);
+    @(posedge CLOCK_50);
+    @(posedge CLOCK_50);
+    @(posedge CLOCK_50);
+    @(posedge CLOCK_50);
+    $stop();
+  end
 endmodule
--- lab4_1/src/bitcounter.sv
+++ lab4_1/src/bitcounter.sv
@@ -7,98 +7,98 @@
 // Count how many ones in an n-bit input A.
 
 module bitcounter (
-	output logic [3:0] result,
-	output logic done,
-	input logic [7:0] A,
-	input logic clk, reset, start
-	);
+  output logic [3:0] result,
+  output logic done,
+  input logic [7:0] A,
+  input logic clk, reset, start
+  );
 
-	logic [7:0] Ar;
-	logic incr_result, rshift_A, init_result, init_A;
-	bitcounter_controller C (.*);
-	bitcounter_datapath D (.*);
+  logic [7:0] Ar;
+  logic incr_result, rshift_A, init_result, init_A;
+  bitcounter_controller C (.*);
+  bitcounter_datapath D (.*);
 
 endmodule
 
 module bitcounter_controller (
-	output logic incr_result, rshift_A, init_result, init_A, done,
-	input logic start, clk, reset,
-	input logic [7:0] Ar
-	);
+  output logic incr_result, rshift_A, init_result, init_A, done,
+  input logic start, clk, reset,
+  input logic [7:0] Ar
+  );
 
-	enum {S1, S2, S3} ps, ns;
+  enum {S1, S2, S3} ps, ns;
 
-	always_comb begin
-		case (ps)
-			S1: ns = start ? S2 : S1;
-			S2: ns = (Ar == 0) ? S3 : S2;
-			S3: ns = start ? S3 : S1;
-		endcase
-	end
+  always_comb begin
+  	case (ps)
+      S1: ns = start ? S2 : S1;
+      S2: ns = (Ar == 0) ? S3 : S2;
+      S3: ns = start ? S3 : S1;
+  	endcase
+  end
 
-	always_comb begin
-		incr_result = (ps == S2) & (ns == S2) & (Ar[0] == 1);
-		init_A = (ps == S1) & (~start);
-		rshift_A = (ps == S2);
-		done = (ps == S3);
-		init_result = (ps == S1);
-	end
+  always_comb begin
+    incr_result = (ps == S2) & (ns == S2) & (Ar[0] == 1);
+    init_A = (ps == S1) & (~start);
+    rshift_A = (ps == S2);
+    done = (ps == S3);
+    init_result = (ps == S1);
+  end
 
-	always_ff @(posedge clk) begin
-		if (reset)
-			ps <= S1;
-		else
-			ps <= ns;
-	end
+  always_ff @(posedge clk) begin
+    if (reset)
+      ps <= S1;
+    else
+      ps <= ns;
+  end
 endmodule
 
 module bitcounter_datapath (
-	output logic [7:0] Ar,
-	output logic [3:0] result,
-	input logic incr_result, rshift_A, init_result, init_A, done,
-	input logic [7:0] A,
-	input logic clk
-	);
+  output logic [7:0] Ar,
+  output logic [3:0] result,
+  input logic incr_result, rshift_A, init_result, init_A, done,
+  input logic [7:0] A,
+  input logic clk
+  );
 
-	always_ff @(posedge clk) begin
-		if (incr_result)
-			result <= result + 4'd1;
-		if (rshift_A)
-			Ar <= Ar >> 8'b1;
-		if (init_A)
-			Ar <= A;
-		if (init_result)
-			result <= 0;
-	end
+  always_ff @(posedge clk) begin
+    if (incr_result)
+      result <= result + 4'd1;
+    if (rshift_A)
+      Ar <= Ar >> 8'b1;
+    if (init_A)
+      Ar <= A;
+    if (init_result)
+      result <= 0;
+  end
 endmodule
 
 module bitcounter_testbench ();
-	logic [3:0] result;
-	logic [7:0] A;
-	logic clk, reset, start, done;
+  logic [3:0] result;
+  logic [7:0] A;
+  logic clk, reset, start, done;
 
-	bitcounter dut (.*);
+  bitcounter dut (.*);
 
-	parameter CLOCK_PERIOD = 100;
-	initial begin
-		clk <= 0;
-		forever #(CLOCK_PERIOD/2) clk <= ~clk;
-	end
+  parameter CLOCK_PERIOD = 100;
+  initial begin
+    clk <= 0;
+    forever #(CLOCK_PERIOD/2) clk <= ~clk;
+  end
 
-	initial begin
-		reset <= 1; @(posedge clk);
-		reset <= 0; start <= 0; A <= 8'b1010_1010; @(posedge clk);
-		start <= 1; @(posedge clk);
-		@(posedge clk);
-		@(posedge clk);
-		@(posedge clk);
-		@(posedge clk);
-		@(posedge clk);
-		@(posedge clk);
-		@(posedge clk);
-		@(posedge clk);
-		@(posedge clk);
-		@(posedge clk);
-		$stop();
-	end
+  initial begin
+    reset <= 1; @(posedge clk);
+    reset <= 0; start <= 0; A <= 8'b1010_1010; @(posedge clk);
+    start <= 1; @(posedge clk);
+    @(posedge clk);
+    @(posedge clk);
+    @(posedge clk);
+    @(posedge clk);
+    @(posedge clk);
+    @(posedge clk);
+    @(posedge clk);
+    @(posedge clk);
+    @(posedge clk);
+    @(posedge clk);
+    $stop();
+  end
 endmodule
